26ffe4087af151b0c9b74ebf8a9fbd143968cef2
Chelsea Huang <cx3huang@uwaterloo.ca>
2021-11-15 11:46:19 -0500

    Finetuned loader and added scrolling to projects

--- js/loader.js
+++ js/loader.js
@@ -1,17 +1,9 @@
 (function(preloader) {
-    window.addEventListener('beforeunload', function(e) {
-        activateLoader();
-      });
 
     window.addEventListener('load', function(e) {
       deactivateLoader();
     });
     
-    function activateLoader() {
-        preloader.style.display = 'flex';
-        preloader.style.opacity = 1;
-      }
-
     function deactivateLoader() {
       /**
        * ensures that the loading animation plays for at least a second to give the
@@ -20,7 +12,7 @@
        */
       setTimeout(function() {
         deactivate();
-      }, 800);
+      }, 750);
   
       function deactivate() {
         preloader.style.opacity = 0;
