aceacbeb4e1973693b8f6cd2a68c301c2ab1d6fa
Chelsea Huang <cx3huang@uwaterloo.ca>
2023-09-19 17:26:16 -0400

    update about

--- react-portfolio/src/components/about/About.jsx
+++ react-portfolio/src/components/about/About.jsx
@@ -38,10 +38,10 @@ const About = () => {
             </div>
 
             <p>
-              My name is Chelsea, currently a Biomedical Engineering student in my 4A term at the University of Waterloo. I am passionate about a lot of STEM subjects and software technologies. In my free time, I love visiting my friends' cats, exploring new restaurants, and playing video games. 
+              My name is Chelsea, currently a Biomedical Engineering student in my 4A term at the University of Waterloo. I'm a passionate believer of lifelong learning and super interested in a lot of STEM subjects and technologies. In my free time, I love visiting my friends' cats, exploring new restaurants, and playing video games. 
             </p>
             <p>
-              I previously received the Lau Engineering Scholarship, numerous program Top Student awards, and the NSERC USRA Award. My current GPA is 94.02%. Some courses I took recently include Pattern Recognition, Control Systems, and Digital Systems.
+              I previously received the NSERC Undergraduate Student Research Award twice, in 2023 and 2021. From 2019-2021, I've also been the recipient of the Lau Engineering Scholarship, numerous program Top Student awards, and merit-based grants. My current GPA is 94.02%. Some courses I am taking at the moment include Image Processing, Deep Learning, and Optimization.
             </p>
             
             <a href='#contact' className='btn btn-primary'>Contact Me</a>
